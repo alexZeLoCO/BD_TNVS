b20f5ed055ee4e327c8639c924240db4074c2a13
Alex <alejandro.rod.lop@gmail.com>
2022-03-17 10:13:17 +0100

    Added universidad files

---
+++ universidad/universidad_datos.sql
@@ -0,0 +1,104 @@
+\c universidad
+
+insert into facultad values (1, 'Facultad de Biologia', 'https://www.uniovi.es/centros/facultades/biologia');
+insert into facultad values (2, 'Facultad de Derecho', 'https://www.uniovi.es/centros/facultades/derecho');
+insert into facultad values (3, 'Facultad de Geologia', 'https://www.uniovi.es/centros/facultades/geologia');
+
+insert into universidad values (100, 1, 'Biologia', 'n', 4, 110);
+insert into universidad values (101, 1, 'Biotecnologia', 'n', 4, 47);
+insert into universidad values (102, 2, 'Derecho', 'n', 4, 350);
+insert into universidad values (103, 3, 'Geologia', 'y', 4, 50);
+insert into universidad values (104, 1, 'Biologia y Tecnologia de Reproduccion', 'y', 1, 20);
+insert into universidad values (105, 1, 'Biotecnologia del Medioambiente', 'y', 2, 20);
+insert into universidad values (106, 2, 'Ley de Proteccion para gente vulnerable', 'n', 1, 25);
+insert into universidad values (107, 3, 'Recursos Geologicos', 'y', 1, 25);
+
+insert into grado values (100, 60, 132, 30);
+insert into grado values (101, 60, 150, 12);
+insert into grado values (102, 60, 120, 54);
+insert into grado values (103, 60, 150, 18);
+
+insert into master values (104, 60);
+insert into master values (105, 120);
+insert into master values (106, 60);
+insert into master values (107, 90);
+
+insert into lugar values (1, 104, 'Facultad de Biologia');
+insert into lugar values (2, 104, 'Facultad de Medicina');
+insert into lugar values (3, 105, 'Facultad de Biologia');
+insert into lugar values (4, 105, 'Universidad en Francia');
+insert into lugar values (5, 105, 'Universidad de Monterrey Mexico');
+insert into lugar values (6, 106, 'Universidad de Derecho');
+insert into lugar values (7, 107, 'Universidad de Geologia');
+
+insert into profesor values (1, 'David', 'Alvarez Fernandez');
+insert into profesor values (2, 'Araceli', 'Anadon Alvarez');
+insert into profesor values (3, 'Carlos', 'Lastra Lopez');
+insert into profesor values (4, 'Carmen', 'Collin Barca');
+insert into profesor values (5, 'Maria Marta', 'Gonzalez Tascon');
+insert into profesor values (6, 'Javier', 'Garcia Amez');
+insert into profesor values (7, 'Ilda', 'Mendez Lopez');
+insert into profesor values (8, 'Jose Maria', 'Roca Martinez');
+insert into profesor values (9, 'Maria Teresa', 'Bulnes Cudeiro');
+insert into profesor values (10, 'Luis Pedro', 'Fernandez Gonzalez');
+insert into profesor values (11, 'Jorge', 'Gallastegui Suarez');
+insert into profesor values (12, 'Olga', 'Garcia Moreno');
+insert into profesor values (13, 'Daniel', 'Garcia Garcia');
+insert into profesor values (14, 'Mario', 'Quevedo de Anta');
+insert into profesor values (15, 'Claudia', 'Gonzalez Toral');
+insert into profesor values (16, 'Maria del Mar', 'Delgado Sanchez');
+
+insert into ensenia values (100, 1);
+insert into ensenia values (100, 2);
+insert into ensenia values (100, 3);
+insert into ensenia values (100, 4);
+insert into ensenia values (101, 13);
+insert into ensenia values (101, 14);
+insert into ensenia values (101, 15);
+insert into ensenia values (101, 16);
+insert into ensenia values (102, 5);
+insert into ensenia values (102, 6);
+insert into ensenia values (102, 7);
+insert into ensenia values (102, 8);
+insert into ensenia values (103, 9);
+insert into ensenia values (103, 10);
+insert into ensenia values (103, 11);
+insert into ensenia values (103, 12);
+
+
+insert into empresa values (1, 'HUCA');
+insert into empresa values (2, 'Serida');
+insert into empresa values (3, 'HUCA');
+insert into empresa values (4, 'Agriculture cooperative');
+insert into empresa values (5, 'Capsa');
+insert into empresa values (6, 'itvasa');
+insert into empresa values (7, 'Mercasturias');
+insert into empresa values (8, 'Minersa');
+insert into empresa values (9, 'BP');
+insert into empresa values (10, 'Tullow Oil');
+
+insert into practica values (100, 1, 1);
+insert into practica values (100, 2, 1);
+insert into practica values (100, 4, 2);
+insert into practica values (101, 14, 3);
+insert into practica values (101, 16, 2);
+insert into practica values (101, 15, 4);
+insert into practica values (102, 5 , 5);
+insert into practica values (102, 6, 7);
+insert into practica values (102, 7, 6);
+insert into practica values (103, 9, 9);
+insert into practica values (103, 10, 10);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
